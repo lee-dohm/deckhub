9ad64e0a7107c52e83daae318ad12cbccd29f7c4
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-09-01 14:44:35 -0700

    Add rendering of some card detail

--- assets/js/components/CardDetail.tsx
+++ assets/js/components/CardDetail.tsx
@@ -1,6 +1,51 @@
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 
-export default class CardDetail extends React.Component<{}, {}> {
+import CardImage from './CardImage'
 
+interface Card {
+  image: string,
+  name: string
+}
+
+interface CardDetailState {
+  card: Card | null
+}
+
+const API = 'http://localhost:4000/api'
+const QUERY = '/cards/ysera'
+
+export default class CardDetail extends React.Component<{}, CardDetailState> {
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      card: null
+    }
+  }
+
+  componentDidMount() {
+    fetch(API + QUERY)
+      .then(response => response.json())
+      .then(card => this.setState({card: card}))
+  }
+
+  render() {
+    const {card} = this.state
+
+    if (!card) {
+      return (
+        <div className='CardDetail'>
+          Loading ...
+        </div>
+      )
+    }
+
+    return (
+      <div className='CardDetail'>
+        <CardImage image={card.image} />
+        <p>{card.name}</p>
+      </div>
+    )
+  }
 }
---
+++ lib/deckhub_api/views/card_view.ex
@@ -0,0 +1,9 @@
+defmodule DeckhubApi.CardView do
+  use DeckhubApi, :view
+
+  def render("show.json", %{card: card}) do
+    card
+    |> Map.from_struct
+    |> Map.drop([:__meta__, :inserted_at, :updated_at])
+  end
+end

7bba23569eb7998e9fae05f4326d4caaead61264
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-09-02 12:14:33 -0700

    :art:

--- lib/deckhub_api/views/card_view.ex
+++ lib/deckhub_api/views/card_view.ex
@@ -3,7 +3,7 @@ defmodule DeckhubApi.CardView do
 
   def render("show.json", %{card: card}) do
     card
-    |> Map.from_struct
+    |> Map.from_struct()
     |> Map.drop([:__meta__, :inserted_at, :updated_at])
   end
 end

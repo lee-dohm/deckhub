d6d14751558242251dfaa1685ad704e737307d80
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-09-03 16:57:12 -0700

    Markdownify the text field in CardDetail

--- lib/deckhub_api/views/card_view.ex
+++ lib/deckhub_api/views/card_view.ex
@@ -1,9 +1,14 @@
 defmodule DeckhubApi.CardView do
   use DeckhubApi, :view
 
+  alias Deckhub.MarkdownEngine
+  alias Deckhub.Text
+
   def render("show.json", %{card: card}) do
     card
     |> Map.from_struct()
     |> Map.drop([:__meta__, :inserted_at, :updated_at])
+    |> Map.update!(:text, &Text.to_markdown/1)
+    |> Map.update!(:text, &MarkdownEngine.render/1)
   end
 end

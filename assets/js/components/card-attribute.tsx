54e3490ab07197c8487ff6531a258e9c06fd8f49
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-09-03 11:04:31 -0700

    Humanize all the things and hide unnecessary fields

--- assets/js/components/card-attribute.tsx
+++ assets/js/components/card-attribute.tsx
@@ -8,10 +8,87 @@ interface CardAttributeProps {
 export default class CardAttribute extends React.Component<CardAttributeProps, {}> {
   public render() {
     return (
-      <tr className={'CardAttribute-' + this.props.name}>
-        <th>{this.props.name}</th>
-        <td>{this.props.value}</td>
+      <tr className={'CardAttribute ' + this.props.name}>
+        <th>{this.humanizeName(this.props.name)}</th>
+        <td>{this.humanizeValue(this.props.name, this.props.value)}</td>
       </tr>
     )
+  }
+
+  private humanizeEnum(value: string): string {
+    if (!value) {
+      return value
+    }
+
+    return value.charAt(0) + value.slice(1).toLowerCase()
+  }
+
+  private humanizeName(name: string): string {
+    if (name === 'dbf_id') {
+      return 'DBF ID'
+    }
+
+    if (name === 'card_id') {
+      return 'Card ID'
+    }
+
+    if (name === 'card_class') {
+      return 'Usable by class'
+    }
+
+    return name.charAt(0).toUpperCase() + name.slice(1)
+  }
+
+  private humanizeSet(value: string): string {
+    switch (value) {
+      case 'BOOMSDAY':
+        return 'The Boomsday Project'
+
+      case 'BRM':
+        return 'Blackrock Mountain'
+
+      case 'EXPERT1':
+        return 'Classic'
+
+      case 'GILNEAS':
+        return 'The Witchwood'
+
+      case 'GVG':
+        return 'Goblins vs. Gnomes'
+
+      case 'ICECROWN':
+        return 'Knights of the Frozen Throne'
+
+      case 'LOOTAPALOOZA':
+        return 'Kobolds & Catacombs'
+
+      case 'NAXX':
+        return 'Naxxramas'
+
+      case 'OG':
+        return 'Whispers of the Old Gods'
+
+      case 'UNGORO':
+        return "Journey to Un'Goro"
+
+      default:
+        return this.humanizeEnum(value)
+    }
+  }
+
+  private humanizeValue(name: string, value: any): any {
+    switch (name) {
+      case 'set':
+        return this.humanizeSet(value)
+
+      case 'type':
+      case 'race':
+      case 'rarity':
+      case 'card_class':
+        return this.humanizeEnum(value)
+
+      default:
+        return value
+    }
   }
 }
--- assets/js/components/card-detail.tsx
+++ assets/js/components/card-detail.tsx
@@ -65,15 +65,35 @@ export default class CardDetail extends React.Component<CardDetailProps, CardDet
     }
 
     return (
-      <div className='CardDetail'>
+      <div className={'CardDetail ' + card.type.toLowerCase()}>
         <div className='col-4 float-left'>
           <CardImage image={card.small_image} />
         </div>
         <div className='col-8 float-left'>
           <table className='data-table'>
             <tbody>
-              <CardAttribute name='armor' value={card.armor} />
+              <CardAttribute name='name' value={card.name} />
+              <CardAttribute name='type' value={card.type} />
+
               <CardAttribute name='cost' value={card.cost} />
+              <CardAttribute name='attack' value={card.attack} />
+              <CardAttribute name='health' value={card.health} />
+              <CardAttribute name='durability' value={card.durability} />
+              <CardAttribute name='armor' value={card.armor} />
+
+              <CardAttribute name='race' value={card.race} />
+              <CardAttribute name='rarity' value={card.rarity} />
+              <CardAttribute name='set' value={card.set} />
+              <CardAttribute name='text' value={card.text} />
+              <CardAttribute name='card_class' value={card.card_class} />
+
+              <CardAttribute name='artist' value={card.artist} />
+              <CardAttribute name='flavor' value={card.flavor} />
+
+              <CardAttribute name='card_id' value={card.card_id} />
+              <CardAttribute name='dbf_id' value={card.dbf_id} />
+              <CardAttribute name='collectible' value={card.collectible} />
+              <CardAttribute name='elite' value={card.elite} />
             </tbody>
           </table>
         </div>

214256df9a1d476f43bade00a92fd170d8679278
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2020-02-17 13:26:00 -0800

    Rebuild webpack pipeline

--- assets/js/components/about-link.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-export default class AboutLink extends React.Component<{}, {}> {
-  public render(): JSX.Element {
-    return <a href='/about'>About</a>
-  }
-}
--- assets/js/components/card-list.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-import { RouteComponentProps } from 'react-router-dom'
-
-export default class CardList extends React.Component<{}, {}> {
-}
